223b798faaa5ef2f6f8733fd274f896b1e15075d
dpw <gugumiao>
2017-12-13 10:22:27 +0800

    ServiceManager ++ accountService

--- src/main/java/com/winstar/user/utils/ServiceManager.java
+++ src/main/java/com/winstar/user/utils/ServiceManager.java
@@ -3,6 +3,7 @@ package com.winstar.user.utils;
 import com.winstar.user.repository.AccessTokenRepository;
 import com.winstar.user.repository.AccountRepository;
 import com.winstar.user.repository.PageViewLogRepository;
+import com.winstar.user.service.AccountService;
 import com.winstar.user.service.PageViewLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ public class ServiceManager {
     public static AccountRepository accountRepository;
     public static PageViewLogRepository pageViewLogRepository;
     public static PageViewLogService pageViewLogService;
+    public static AccountService accountService;
 
     @Autowired
     public void setAccessTokenRepository(AccessTokenRepository accessTokenRepository) {
@@ -32,5 +34,9 @@ public class ServiceManager {
     @Autowired
     public void setPageViewLogService(PageViewLogService pageViewLogService) {
         ServiceManager.pageViewLogService = pageViewLogService;
+    }
+    @Autowired
+    public void setAccountService(AccountService accountService) {
+        ServiceManager.accountService = accountService;
     }
 }
