21227b2d52766e170a2f169349d5a6e3ef64ae66
qq296141266 <296141266@qq.com>
2017-12-14 17:55:59 +0800

    查询优惠券可用否

--- src/main/java/com/winstar/user/service/OneMoneyCouponRecordService.java
+++ src/main/java/com/winstar/user/service/OneMoneyCouponRecordService.java
@@ -21,23 +21,25 @@ public class OneMoneyCouponRecordService {
 
     /**
      * 查询用户是否有购买一元券的权限
+     *
      * @param accountId 用户id
      * @return true 有 false 无
      */
-    public boolean checkBuyAuth(String accountId){
-        List<OneMoneyCouponRecord> list=oneMoneyCouponRecordRepository.findByAccountId(accountId);
-        if(list.size()>0) return  false;
+    public boolean checkBuyAuth(String accountId) {
+        List<OneMoneyCouponRecord> list = oneMoneyCouponRecordRepository.findByAccountId(accountId);
+        if (list.size() > 0) return false;
         return true;
     }
 
     /**
      * 用户购买1元油券
+     *
      * @param accountId
      * @return
      */
-    public OneMoneyCouponRecord changeStatus(String accountId){
-        List<OneMoneyCouponRecord> list=oneMoneyCouponRecordRepository.findByAccountId(accountId);
-        for(OneMoneyCouponRecord record:list){
+    public OneMoneyCouponRecord changeStatus(String accountId) {
+        List<OneMoneyCouponRecord> list = oneMoneyCouponRecordRepository.findByAccountId(accountId);
+        for (OneMoneyCouponRecord record : list) {
             record.setStatus(1);
             oneMoneyCouponRecordRepository.save(record);
         }
@@ -46,16 +48,17 @@ public class OneMoneyCouponRecordService {
 
     /**
      * 处理违法后生成可购买1元油券的权限
+     *
      * @param accountId 用户Id
-     * @param orderId  违法id
+     * @param orderId   违法id
      * @return
      */
-    public OneMoneyCouponRecord insertRecord(String accountId,String orderId){
-        OneMoneyCouponRecord record=new OneMoneyCouponRecord();
+    public OneMoneyCouponRecord insertRecord(String accountId, String orderId) {
+        OneMoneyCouponRecord record = new OneMoneyCouponRecord();
         record.setStatus(0);
         record.setAccountId(accountId);
         record.setOrderId(orderId);
-        OneMoneyCouponRecord oneMoneyCouponRecord= oneMoneyCouponRecordRepository.save(record);
+        OneMoneyCouponRecord oneMoneyCouponRecord = oneMoneyCouponRecordRepository.save(record);
         return oneMoneyCouponRecord;
     }
 
