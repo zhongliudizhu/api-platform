f86f345e24d4425756fd5c17067e7be01d0b4be2
zhanglin <zl1234>
2017-12-19 17:03:40 +0800

    微信支付

--- src/main/java/com/winstar/oil/service/SendOilCouponService.java
+++ src/main/java/com/winstar/oil/service/SendOilCouponService.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,7 @@ import java.util.*;
 /**
  * Created by zl on 2017/12/13
  */
+@Service
 public class SendOilCouponService {
 
     private static final Logger logger = LoggerFactory.getLogger(SendOilCouponService.class);
