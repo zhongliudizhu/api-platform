6df0c680ec852841aa18807629f17d779e13937f
dpw <gugumiao>
2017-12-20 11:23:43 +0800

    添加领券资格

--- src/main/java/com/winstar/user/service/AccountService.java
+++ src/main/java/com/winstar/user/service/AccountService.java
@@ -3,14 +3,47 @@ package com.winstar.user.service;
 import com.winstar.exception.NotFoundException;
 import com.winstar.exception.NotRuleException;
 import com.winstar.user.entity.Account;
+import com.winstar.user.param.AccountParam;
 import com.winstar.user.utils.ServiceManager;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @Service
 public class AccountService {
+    /**
+     * 根据openid获取用户accountId 如果没有用户，注册用户
+     *
+     * @param openid openid
+     * @return accountId
+     */
+    public String findAccountIdByOpenid(String openid) {
+        Account account = ServiceManager.accountRepository.findByOpenid(openid);
+        if (null == account) {
+            account = ServiceManager.accountService.createAccount(new AccountParam(openid, "", ""));
+            return account.getId();
+        }
+        return account.getId();
+    }
 
+    /**
+     * 创建用户信息
+     *
+     * @param accountParam accountParam
+     * @return Account Account
+     */
+    public Account createAccount(@RequestBody AccountParam accountParam) {
+        Account account;
+        account = new Account();
+        account.setCreateTime(new Date());
+        account.setUpdateTime(new Date());
+        account.setHeadImgUrl(accountParam.getHeadImgUrl());
+        account.setOpenid(accountParam.getOpenid());
+        account.setNickName(accountParam.getNickName());
+        return ServiceManager.accountRepository.save(account);
+    }
 
     /**
      * 根据id 获取账号信息
