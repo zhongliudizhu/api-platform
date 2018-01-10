6f403db9b101ef96aa1f22cf7572026862db03b6
shog808 <xiaoshao808@126.com>
2018-01-10 14:40:29 +0800

    每月一次只查询未关闭的订单

--- src/main/java/com/winstar/order/repository/OilOrderRepository.java
+++ src/main/java/com/winstar/order/repository/OilOrderRepository.java
@@ -41,6 +41,6 @@ public interface OilOrderRepository extends JpaRepository<OilOrder,String> {
     /*
     * 用户一段时间某活动的订单
     * */
-    @Query("select o from OilOrder o where o.accountId=?1 and o.activityId=?2 and o.createTime between ?3 and ?4")
+    @Query("select o from OilOrder o where o.isAvailable='0'and o.accountId=?1 and o.activityId=?2 and o.createTime between ?3 and ?4")
     List<OilOrder> findByAccountIdAndAndActivityId(String accountId, String activityId, Date begin, Date end);
 }
