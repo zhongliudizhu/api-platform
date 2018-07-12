0bd248d9598308628215293607a1db46e370c5ad
dpw <499909744@qq.com>
2018-07-12 16:58:51 +0800

    把裂变的都删了

--- src/main/java/com/winstar/fission/repository/ActivityQuestionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.winstar.fission.repository;
-
-import com.winstar.fission.entity.ActivityQuestions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityQuestionsRepository extends JpaRepository<ActivityQuestions, Integer> {
-    int countByActivityId(String activityId);
-}
