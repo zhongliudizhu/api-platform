f2806eba6e86e2ce8e2a2873cd4b0a5b01ad70b6
zhanglin <zl1234>
2018-11-15 18:01:00 +0800

    随机查询券

--- src/main/java/com/winstar/oil/repository/OilCouponRepository.java
+++ src/main/java/com/winstar/oil/repository/OilCouponRepository.java
@@ -19,7 +19,7 @@ public interface OilCouponRepository extends JpaSpecificationExecutor<OilCoupon>
 
     Page<OilCoupon> findByPanAmtAndOilState(Double panAmt, String oilState, Pageable pageable);
 
-    @Query(value = "select * from cbc_oil_coupon where pan_amt=?1 and oil_state=0 order by RAND() limit 50", nativeQuery = true)
+    @Query(value = "SELECT * FROM (SELECT * FROM cbc_oil_coupon where oil_state=0 and pan_amt=?1) t ORDER BY RAND() LIMIT 50", nativeQuery = true)
     List<OilCoupon> findRandomOilCoupons(Double panAmt);
 
 }
