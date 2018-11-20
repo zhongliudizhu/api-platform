5f648e40ded3389303fcb523819ae4b4a913fb05
499909744 <499909744@qq.com>
2018-11-20 18:38:54 +0800

    周末分享生成红包机会; 领取红包;

--- src/main/java/com/winstar/order/repository/OilOrderRepository.java
+++ src/main/java/com/winstar/order/repository/OilOrderRepository.java
@@ -21,34 +21,34 @@ public interface OilOrderRepository extends JpaRepository<OilOrder, String> {
     OilOrder findBySerialNumber(String serialNo);
 
     /*
-    * 查询用户油券订单
-    * */
+     * 查询用户油券订单
+     * */
     List<OilOrder> findByAccountId(String accountId);
 
     @Query("select o from OilOrder o where o.isAvailable=?1 and o.status=?2 and o.createTime between ?3 and ?4")
     List<OilOrder> findByIsAvailableAndStatusAndCreateTimeBetween(String isAvailable, Integer status, Date begin, Date end);
 
     /*
-    * 每天某商品购买的总数量
-    * */
+     * 每天某商品购买的总数量
+     * */
     @Query("select o from OilOrder o where o.isAvailable=?1 and o.itemId=?2  and o.createTime between ?3 and ?4")
     List<OilOrder> findByIsAvailableAndItemIdAndCreateTime(String isAvailable, String itemId, Date begin, Date end);
 
     /*
-    * 用户一段时间的订单
-    * */
+     * 用户一段时间的订单
+     * */
     @Query("select o from OilOrder o where o.accountId=?1 and o.itemId=?2  and o.isAvailable = '0' and o.createTime between ?3 and ?4")
     List<OilOrder> findByAccountIdAndAndItemId(String accountId, String itemId, Date begin, Date end);
 
     /*
-    * 用户一段时间某活动的订单
-    * */
+     * 用户一段时间某活动的订单
+     * */
     @Query("select o from OilOrder o where o.isAvailable='0' and o.accountId=?1 and o.activityId=?2 and o.createTime between ?3 and ?4")
     List<OilOrder> findByAccountIdAndActivityId(String accountId, String activityId, Date begin, Date end);
 
     /*
-   * 一段时间某活动的订单
-   * */
+     * 一段时间某活动的订单
+     * */
     @Query("select o from OilOrder o where o.isAvailable='0' and o.activityId=?1 and o.createTime between ?2 and ?3")
     List<OilOrder> findByActivityId(String activityId, Date begin, Date end);
 
@@ -69,4 +69,6 @@ public interface OilOrderRepository extends JpaRepository<OilOrder, String> {
 
     @Query(value = "SELECT count(0) FROM cbc_oil_order o WHERE o.is_available = '0'AND o.pay_status='1'AND o.account_id =?1", nativeQuery = true)
     long countByStatusAndAccountIdAndIsAvailable(String accountId);
+
+    long countByIdAndActivityIdAndIsAvailableAndStatus(String id, String activityId, String isAvaliable, Integer status);
 }
--- src/test/java/com/PageViewLog/test.java
+++ src/test/java/com/PageViewLog/test.java
@@ -11,7 +11,8 @@ import com.winstar.utils.AESUtil;
  */
 public class test {
     public static void main(String[] args)throws Exception{
-       System.out.println(AESUtil.decrypt("E7B7F1EFD1DB0CD11D43A5C92E991F7C8FB1E593DAE063583B7B8EF7052A2C8A",AESUtil.dekey));
-        System.out.println(AESUtil.decrypt("BBEF0F8EA1A73FCAFEA3746D858424784A407A6ADA85A59DEB860376F92E6120",AESUtil.dekey));
+        System.out.println(AESUtil.decrypt("A1DAC05B97DB91D5EC033723C2BC05DAE826DCA97071B71B46B24B8F09F7FFB0",AESUtil.dekey));
+       System.out.println(AESUtil.decrypt("1AF29CA6C6254DD2DE97901AE8EAF57432A8588539E91F6480231646D50BB8D0",AESUtil.dekey));
+        System.out.println(AESUtil.decrypt("2F3023571098DD08A565FDAC2D8B3E68EDD312239AB0A8027CF3D38962D3566B",AESUtil.dekey));
     }
 }
