7652688e0d94b5e37936af50f5b3d5b0dd6bcbc6
bestUU <lyyllxt@163.com>
2019-05-30 11:50:43 +0800

    我的可用优惠券

--- src/main/java/com/winstar/costexchange/controller/AccountCouponController.java
+++ src/main/java/com/winstar/costexchange/controller/AccountCouponController.java
@@ -2,16 +2,16 @@ package com.winstar.costexchange.controller;
 
 import com.winstar.costexchange.entity.AccountCoupon;
 import com.winstar.costexchange.repository.AccountCouponRepository;
-import com.winstar.costexchange.utils.SignUtil;
+import com.winstar.costexchange.service.AccountCouponService;
 import com.winstar.costexchange.vo.AccountCouponVo;
 import com.winstar.shop.entity.Goods;
 import com.winstar.shop.repository.GoodsRepository;
 import com.winstar.utils.WebUitl;
 import com.winstar.vo.Result;
 import groovy.util.logging.Slf4j;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -32,22 +31,23 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/cbc/cost")
 @Slf4j
+@AllArgsConstructor
 public class AccountCouponController {
 
     private static final Logger logger = LoggerFactory.getLogger(AccountCouponController.class);
 
-    @Autowired
     AccountCouponRepository accountCouponRepository;
 
-    @Autowired
     GoodsRepository goodsRepository;
+
+    AccountCouponService accountCouponService;
 
     /**
      * 查询优惠券列表
      */
     @RequestMapping(value = "getCoupons", method = RequestMethod.GET)
-    public Result getCoupons(HttpServletRequest request, @RequestParam(defaultValue = "0") Integer nextPage, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "normal") String state){
-        if(!state.equals("normal") && !state.equals("used") && !state.equals("expired")){
+    public Result getCoupons(HttpServletRequest request, @RequestParam(defaultValue = "0") Integer nextPage, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "normal") String state) {
+        if (!state.equals("normal") && !state.equals("used") && !state.equals("expired") && !state.equals("locked")) {
             logger.info("状态值错误！");
             return Result.fail("state_not_auth", "状态值错误！");
         }
@@ -65,30 +65,33 @@ public class AccountCouponController {
      * 查询我的可用优惠券列表
      */
     @RequestMapping(value = "getUsableCoupons", method = RequestMethod.GET)
-    public Result getMyUsableCoupons(HttpServletRequest request, @RequestParam String shopId){
+    @SuppressWarnings("unchecked")
+    public Result getMyUsableCoupons(HttpServletRequest request, @RequestParam String shopId) {
         String accountId = (String) request.getAttribute("accountId");
         List<AccountCoupon> accountCoupons = accountCouponRepository.findByAccountIdAndShowStatus(accountId, "yes");
-        if(ObjectUtils.isEmpty(accountCoupons)){
+        if (ObjectUtils.isEmpty(accountCoupons)) {
             logger.info("用户无优惠券！");
             return Result.fail("coupons_not_found", "用户无优惠券！");
         }
         Goods goods = goodsRepository.findOne(shopId);
-        if (goods == null){
+        if (goods == null) {
             logger.info("商品不存在！");
             return Result.fail("shop_not_found", "商品不存在！");
         }
         String couponIds = accountCoupons.stream().map(AccountCoupon::getCouponId).collect(Collectors.joining(","));
         logger.info("couponIds:" + couponIds);
-        Map<String, String> reqMap = new HashMap<>();
-        reqMap.put("ids", couponIds);
-        reqMap.put("itemAmount", goods.getPrice().toString());
-        reqMap.put("merchant", SignUtil.merchant);
-        ResponseEntity<Map> resp = new RestTemplate().getForEntity("http://localhost:12002/api/v1/coupon/verify/verify?" + SignUtil.getParameters(reqMap), Map.class);
+        ResponseEntity<Map> resp = accountCouponService.checkCoupon(couponIds, goods.getPrice().toString());
         logger.info("map:" + resp.getBody().toString());
+        Map map = resp.getBody();
+        if (!"SUCCESS".equals(map.get("code"))) {
+            List<String> list = (List<String>) map.get("data");
+            List<AccountCoupon> couponList = accountCoupons.stream().filter(e -> !list.contains(e.getCouponId())).collect(Collectors.toList());
+            return Result.success(getAccountCoupons(couponList));
+        }
         return Result.success(getAccountCoupons(accountCoupons));
     }
 
-    private List<AccountCouponVo> getAccountCoupons(List<AccountCoupon> accountCoupons){
+    private List<AccountCouponVo> getAccountCoupons(List<AccountCoupon> accountCoupons) {
         List<AccountCouponVo> accountCouponVos = new ArrayList<>();
         //封装话费兑换优惠券
         AccountCouponVo accountCouponVo_cost = new AccountCouponVo();
