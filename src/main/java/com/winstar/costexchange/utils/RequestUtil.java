e78d27d8a1280b6b38e86b1064093edfab057a52
qyc <m15691755590_2@163.com>
2019-08-15 17:13:13 +0800

    发送优惠券回填手机号

--- src/main/java/com/winstar/costexchange/utils/RequestUtil.java
+++ src/main/java/com/winstar/costexchange/utils/RequestUtil.java
@@ -26,18 +26,22 @@ public class RequestUtil {
     private static final Logger logger = LoggerFactory.getLogger(RequestUtil.class);
 
     @Async
-    public static Map post(String url, Map<String, String> reqMap){
+    public static Map post(String url, Map<String, String> reqMap) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(JSON.toJSONString(reqMap),headers);
+        HttpEntity<String> entity = new HttpEntity<>(JSON.toJSONString(reqMap), headers);
         ResponseEntity<Map> resp = new RestTemplate().exchange(url, HttpMethod.POST, entity, Map.class);
         return resp.getBody();
     }
 
     public static List<AccountCoupon> getAccountCoupons(String coupons, String type, String accountId, String activityId, RedisTools redisTools) {
+        return getAccountCoupons(coupons, type, accountId, activityId, redisTools, null);
+    }
+
+    public static List<AccountCoupon> getAccountCoupons(String coupons, String type, String accountId, String activityId, RedisTools redisTools, String phone) {
         List<CouponVo> couponVos = JSON.parseArray(coupons, CouponVo.class);
         List<AccountCoupon> accountCoupons = new ArrayList<>();
-        for(CouponVo couponVo : couponVos){
+        for (CouponVo couponVo : couponVos) {
             AccountCoupon accountCoupon = new AccountCoupon();
             accountCoupon.setCouponId(couponVo.getId());
             accountCoupon.setAmount(couponVo.getAmount());
@@ -52,11 +56,12 @@ public class RequestUtil {
             accountCoupon.setCreatedAt(new Date());
             accountCoupon.setAccountId(accountId);
             accountCoupon.setType(type);
+            accountCoupon.setPhone(phone);
             accountCoupon.setTemplateId(couponVo.getTemplateId());
             accountCoupon.setActivityId(activityId);
-            if (!ObjectUtils.isEmpty(redisTools)){
+            if (!ObjectUtils.isEmpty(redisTools)) {
                 String switchCard = (String) redisTools.get(couponVo.getTemplateId() + "_cardable");
-                if(!StringUtils.isEmpty(switchCard) && switchCard.equals("yes")){
+                if (!StringUtils.isEmpty(switchCard) && switchCard.equals("yes")) {
                     accountCoupon.setCardPackageId((String) redisTools.get(couponVo.getTemplateId() + "_cardId"));
                 }
             }
@@ -65,5 +70,4 @@ public class RequestUtil {
         }
         return accountCoupons;
     }
-
 }
