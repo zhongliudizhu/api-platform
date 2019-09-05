e65d7054668c6e2d707e17bf7047f927d79309c7
uu <lyyllxt@163.com>
2019-09-05 14:26:51 +0800

    去除页面访问日志

--- src/main/java/com/winstar/user/controller/PageViewLogController.java
+++ src/main/java/com/winstar/user/controller/PageViewLogController.java
@@ -1,14 +1,9 @@
 package com.winstar.user.controller;
 
-import com.winstar.exception.NotRuleException;
 import com.winstar.user.entity.PageViewLog;
-import com.winstar.user.utils.ServiceManager;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -22,14 +17,13 @@ public class PageViewLogController {
     /**
      * 添加页面访问日志
      *
-     * @param pageViewLog pageViewLog
      * @return PageViewLog
-     * @throws NotRuleException NotRuleException
      */
     @PostMapping(value = "/add", produces = "application/json")
-    public PageViewLog add(@RequestBody PageViewLog pageViewLog, HttpServletRequest request) throws NotRuleException {
-        String accountId = ServiceManager.accountService.getAccountId(request);
-        pageViewLog.setAccountId(accountId);
-        return ServiceManager.pageViewLogService.savePageViewLog(pageViewLog);
+    public PageViewLog add() {
+//        String accountId = ServiceManager.accountService.getAccountId(request);
+//        pageViewLog.setAccountId(accountId);
+//        return ServiceManager.pageViewLogService.savePageViewLog(pageViewLog);
+        return null;
     }
 }
