1f33feed470895ec749393fa745738cac0f7128f
zhanglin <172810573@qq.com>
2019-09-20 11:35:10 +0800

    优惠券赠送记录

--- src/main/java/com/winstar/redis/RedisConfig.java
+++ src/main/java/com/winstar/redis/RedisConfig.java
@@ -10,8 +10,10 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -79,7 +81,8 @@ public class RedisConfig{
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisTemplate template) {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
