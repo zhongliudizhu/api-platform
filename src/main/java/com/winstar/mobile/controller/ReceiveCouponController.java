6d5758f35cb304bfab644734319437b963261f6a
shibinbin <15596186979@163.com>
2020-04-22 14:13:03 +0800

    提交实体

---
+++ src/main/java/com/winstar/mobile/controller/ReceiveCouponController.java
@@ -0,0 +1,14 @@
+package com.winstar.mobile.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/cbc/mobile")
+@AllArgsConstructor
+public class ReceiveCouponController {
+
+}
