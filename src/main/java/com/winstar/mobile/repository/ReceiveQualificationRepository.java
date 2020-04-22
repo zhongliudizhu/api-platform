3a2bb408354ae09f50e1c2e454187db8c3dd7d53
shibinbin <15596186979@163.com>
2020-04-22 14:04:05 +0800

    Revert "提交repository"
    This reverts commit 7bfd2fde

--- src/main/java/com/winstar/mobile/repository/ReceiveQualificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.winstar.mobile.repository;
-
-import com.winstar.mobile.entity.ReceiveQualification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceiveQualificationRepository extends JpaRepository<ReceiveQualification, String> {
-}
