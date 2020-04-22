7bfd2fde5272b538213abf9aca66ebd0206c38e8
shibinbin <15596186979@163.com>
2020-04-22 13:49:02 +0800

    提交repository

---
+++ src/main/java/com/winstar/mobile/repository/ReceiveQualificationRepository.java
@@ -0,0 +1,7 @@
+package com.winstar.mobile.repository;
+
+import com.winstar.mobile.entity.ReceiveQualification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiveQualificationRepository extends JpaRepository<ReceiveQualification, String> {
+}
