dab8370266fcc7f72637a7c2d9375f8f07c2386b
qyc <m15691755590_2@163.com>
2020-04-23 18:02:53 +0800

    修改图片验证码

--- src/main/java/com/winstar/mobile/controller/ValidationController.java
+++ src/main/java/com/winstar/mobile/controller/ValidationController.java
@@ -3,8 +3,6 @@ package com.winstar.mobile.controller;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.kaptcha.exception.KaptchaRenderException;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.winstar.exception.NotFoundException;
-import com.winstar.exception.NotRuleException;
 import com.winstar.mobile.domain.CmResult;
 import com.winstar.mobile.domain.SendMessageRequestDomain;
 import com.winstar.mobile.domain.VerifyRequestDomain;
@@ -18,12 +16,13 @@ import com.winstar.vo.Result;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
@@ -96,9 +95,7 @@ public class ValidationController {
      * 获取图片验证码
      */
     @RequestMapping(value = "/render", method = RequestMethod.POST)
-    public void render(HttpServletResponse response, HttpServletRequest request) throws NotRuleException, NotFoundException {
-        String accountId = accountService.getAccountId(request);
-        if (StringUtils.isEmpty(accountId)) throw new NotFoundException("validation");
+    public void render(HttpServletResponse response){
         response.setDateHeader("Expires", 0L);
         response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
         response.addHeader("Cache-Control", "post-check=0, pre-check=0");
