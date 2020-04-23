3e675af0a5da1871b87cab9648a1f17834d473e0
shibinbin <15596186979@163.com>
2020-04-23 10:38:50 +0800

    以openid来领券

--- src/main/java/com/winstar/mobile/controller/ReceiveCouponController.java
+++ src/main/java/com/winstar/mobile/controller/ReceiveCouponController.java
@@ -8,6 +8,9 @@ import com.winstar.mobile.domain.ReceiveDomain;
 import com.winstar.mobile.entity.ReceiveQualification;
 import com.winstar.mobile.repository.ReceiveQualificationRepository;
 import com.winstar.redis.RedisTools;
+import com.winstar.user.entity.Account;
+import com.winstar.user.service.AccountService;
+import com.winstar.user.service.FansService;
 import com.winstar.vo.Result;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,44 +21,49 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
-@RequestMapping("/api/v1/cbc/mobile")
+@RequestMapping("/api/v1/noAuth/")
 @AllArgsConstructor
 public class ReceiveCouponController {
 
     private RedisTools redisTools;
     private AccountCouponService accountCouponService;
     private ReceiveQualificationRepository qualificationRepository;
+    private AccountService accountService;
+    private FansService fansService;
 
-    @PostMapping("/receiveCoupons")
-    public Result receiveCoupons(@RequestBody @Valid ReceiveDomain receiveDomain, HttpServletRequest request) {
+    @PostMapping("mobile/receiveCoupons")
+    public Result receiveCoupons(@RequestBody @Valid ReceiveDomain receiveDomain) {
         log.info("入参信息为 {}", JSON.toJSONString(receiveDomain));
-        String accountId = (String) request.getAttribute("accountId");
-        if (!redisTools.setIfAbsent(accountId + "_receive", 3)) {
-            log.info("点击频率太高了吧，请慢点！");
-            return Result.fail("click_too_fast", "点击太快，请慢点");
+        Map fans = fansService.getFansInfo(receiveDomain.getOpenId(), false);
+        if (ObjectUtils.isEmpty(fans)) {
+            log.info("openId无效！");
+            return Result.fail("openId_is_invalid", "openId无效！");
         }
+        if ("0".equals(fans.get("subscribe").toString())) {
+            log.info("用户未关注！");
+            return Result.fail("user_not_subscribe", "用户未关注！");
+        }
+        Account account = accountService.getAccountOrCreateByOpenId(receiveDomain.getOpenId(), null, null);
         String validateKey = receiveDomain.getPhone();
         Object data = redisTools.get(validateKey);
         log.info("redis中的 data is {}", data);
         if (ObjectUtils.isEmpty(data) || !("YES").equalsIgnoreCase(data.toString())) {
             log.info("验证资格失败");
-            redisTools.remove(accountId + "_receive");
-            return Result.fail("validate_failed", "您无资格领券，请先办理套餐");
+            return Result.fail("validate_failed", "当前手机号未办理套餐");
         }
         String[] templates = receiveDomain.getTemplateId().split(",");
         for (String s : templates) {
-            SendCouponDomain domain = new SendCouponDomain(s, accountId, "mobile_12.2", "1", null, null);
+            SendCouponDomain domain = new SendCouponDomain(s, account.getId(), "mobile_12.2", "1", null, null);
             log.info("发放优惠券 。。 templateId is{}", s);
             List<AccountCoupon> couponList = accountCouponService.sendCoupon(domain, null);
             if (CollectionUtils.isEmpty(couponList)) {
-                redisTools.remove(accountId + "_receive");
                 return Result.fail("give_coupon_failed", "发券失败");
             }
         }
--- src/main/java/com/winstar/mobile/domain/ReceiveDomain.java
+++ src/main/java/com/winstar/mobile/domain/ReceiveDomain.java
@@ -8,8 +8,9 @@ public class ReceiveDomain {
 
     @NotEmpty(message = "模板id不能为空")
     private String templateId;
-
     @NotEmpty(message = "手机号不能为空")
     private String phone;
+    @NotEmpty(message = "openId不能为空")
+    private String openId;
 
 }
